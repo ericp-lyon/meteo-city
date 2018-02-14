0080d10e11e0f8d17ae5873a7eaca05583979b42
eric <eric.pozo@sfr.fr>
2018-02-14 21:39:13 +0100

    create component

--- src/components/polution/polution.component.js
+++ src/components/polution/polution.component.js
@@ -1,5 +1,5 @@
-import {Component} from "./../component.component";
-import {default as template} from "./polution.component.html";
+import { Component } from "./../component.component";
+import { default as template } from "./polution.component.html";
 
 /**
  * @type {Polution}
@@ -7,16 +7,65 @@ import {default as template} from "./polution.component.html";
 
 export class Polution extends Component {
 
- /**
- * @constructor
- */
-    constructor (){
-        
+    /**
+    * @constructor
+    * @param {Model} polution
+    * @param {Api} api
+    * @param {Hydrator} hydrator
+    */
+    constructor(polution, api, hydrator) {
+
         super();
         this.selector = "polution";
         this.template = template;
+
+        /**
+         * @returns {Model} polution
+         */
+        this.getModel = () => {
+            return polution;
+        }
+        /**
+         * @returns {Api} api
+         */
+
+        this.getApi = () => {
+            return api;
+        }
+        /**
+         * @returns {Hydrator} hydrator
+         */
+        this.getHydrator = () => {
+            return hydrator;
+        }
+        this.ipBasedApi();
     }
 
+    ipBasedApi () {
+        let xhr = new XMLHttpRequest;
+        xhr.open("GET", this.getApi().getEndPoint());
+        xhr.onload = xhr.onerror = xhr.onabort = () => {
 
-    
+            let response = window.JSON.parse(xhr.response);
+               
+            this.getHydrator().hydrate(
+                    this.getModel(),
+                    window.JSON.parse(xhr.response)
+                );
+            this.render();
+         
+        }
+        xhr.send();
+    } 
+    render() {
+
+
+        let elements = super.render([this.getModel()]);
+        for (let i = 0, l = elements.length; i<l ; i++) {
+
+            window.componentHandler.downgradeElements(elements[i]);
+            window.componentHandler.upgradeDom();
+        }
+
+    }
 }
