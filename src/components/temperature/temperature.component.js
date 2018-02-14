8556e41fcb281ac5a7dae51a17060907ff2635b7
eric <eric.pozo@sfr.fr>
2018-02-14 21:40:41 +0100

    update param

--- src/components/temperature/temperature.component.js
+++ src/components/temperature/temperature.component.js
@@ -9,14 +9,31 @@ export class Temperature extends Component {
 
  /**
  * @constructor
+ * @param {City} city
  */
-    constructor (){
+    constructor (city){
         
         super();
         this.selector = "temperature";
         this.template = template;
+    /**
+    * @returns{City} city
+    */
+        this.getModel = () =>{
+        return city;
+        }
+         //bind() qui correspond à la fonction
+        city.bind(                     
+            this.render.bind(this) // bind du contexte
+        );
+ 
     }
 
-
-    
+    /**
+     * @returns {HTMLElmentCollection}
+     */
+    render(){
+        super.render([this.getModel()]);
+    }
+   
 }
